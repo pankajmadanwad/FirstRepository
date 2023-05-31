6b8ae528de2a603dc72e94ca51a12039a3e581ff
pankajmadanwad <135091172+pankajmadanwad@users.noreply.github.com>
2023-05-31 09:34:40 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+class Another
+{
+   public static void main(String[] arg){
+    System.out.println("Hello Java1");
+}
+  
+}
+
+class Another2
+{
+   public static void main(String[] arg){
+    System.out.println("Hello Java2");
+}
+  
+}
